98344f3c784ba71926e15c14f69038a4ed700734
dealnotedev <dealnote.dev@gmail.com>
2025-10-07 17:24:02 +0300

    fix for new intl

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hunt_stats/l10n/app_localizations.dart';
 
 extension ContextExt on BuildContext {
   AppLocalizations get localizations => AppLocalizations.of(this)!;
